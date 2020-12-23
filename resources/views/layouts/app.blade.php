457c928182758189a360d0a81682b3894e58ccaa
FrancescoDale <fd060862@gmail.com>
2020-12-23 08:43:01 +0100

    chiave in web.php e stampa condizionale in base al tipo dei formati

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
 
             <main>
                 @yield('content')
-
+                @yield('cards')
             </main>
 
         </div>
--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -4,4 +4,14 @@
 
 @section('content')
     <h1>Lista prodotti</h1>
+    <h2>LE LUNGHE</h2>
+    <div class="cardbox">
+        @foreach ($pasta as $value)
+            @if($value['tipo'] === 'lunga')
+            <div class="card">
+                <img src=" {{ $value['src'] }} " alt="pasta">
+            </div>
+            @endif
+        @endforeach
+    </div>
 @endsection
